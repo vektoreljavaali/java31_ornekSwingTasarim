286330a7e5bec8142ad9e4499bb088a7e789a24e
vektorel <vektorel@lab9-ogr>
2018-03-19 21:25:29 +0300

    hrhg

---
+++ src/com/models/tblmusteri.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.models;
+
+/**
+ *
+ * @author vektorel
+ */
+public class tblmusteri {
+    
+    private String adi;
+    private String soyadi;
+    private String telefonu;
+    private String adresi;
+
+    public String getAdi() {
+        return adi;
+    }
+
+    public void setAdi(String adi) {
+        this.adi = adi;
+    }
+
+    public String getSoyadi() {
+        return soyadi;
+    }
+
+    public void setSoyadi(String soyadi) {
+        this.soyadi = soyadi;
+    }
+
+    public String getTelefonu() {
+        return telefonu;
+    }
+
+    public void setTelefonu(String telefonu) {
+        this.telefonu = telefonu;
+    }
+
+    public String getAdresi() {
+        return adresi;
+    }
+
+    public void setAdresi(String adresi) {
+        this.adresi = adresi;
+    }
+    
+    
+    
+            
+    
+}
